74c3267f6e653c4f3ece49c849d1eb64e5db1d56
RodrigoMaga <rgn.nunnes@gmail.com>
2026-01-15 17:13:00 -0300

    user entity & resource

---
+++ src/main/java/com/rodmag/ordermanagement/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.rodmag.ordermanagement.resources;
+
+import com.rodmag.ordermanagement.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Rodrigo", "rodrigo@gmail.com", "123456789", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
